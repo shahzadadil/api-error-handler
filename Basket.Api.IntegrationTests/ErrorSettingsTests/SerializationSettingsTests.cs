29132e9b40cf75c058eebd6fbcbd90a095e1b732
Md Shahzad Adil <shahzad.adil00@gmail.com>
2019-06-14 00:57:44 +0530

    Releases/alpha (#2)
    * Allow settings to be used and add some more error types
    
    * Add api framework test cases
    
    * updated package version
    
    * Add icon and packaging information
    
    * Add intergation tests and feature to add exception details
    
    * Add integration tests for api error settings

---
+++ Basket.Api.IntegrationTests/ErrorSettingsTests/SerializationSettingsTests.cs
@@ -0,0 +1,55 @@
+﻿using Basket.Api.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Basket.Api.IntegrationTests.ErrorSettingsTests
+{
+    [TestClass]
+    public class SerializationSettingsTests : ApiTestBase
+    {
+        protected override string _ControllerRoute
+        {
+            get { return "error"; }
+        }
+
+        [TestMethod]
+        public async Task UseCamelCase_DefaultSwitchedOn_ResponseInCamelCase()
+        {
+            //// Arrange
+            CreateClient();
+
+            //// Act
+            var response = await _ApiClient.GetAsync($"{_BasePath}/random");
+
+            //// Assert
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            Assert.IsTrue(responseContent.Contains("\"statusCode\":"));
+            Assert.IsFalse(responseContent.Contains("\"StatusCode\":"));
+        }
+
+        [TestMethod]
+        public async Task UseCamelCase_SwitchedOff_ResponseInTitleCase()
+        {
+            //// Arrange
+            var apiErrorSettings = new ApiErrorSettings
+            {
+                Serialization = new SerializationSettings
+                {
+                    UseCamelCase = false
+                }
+            };
+
+            CreateClient(apiErrorSettings);
+
+            //// Act
+            var response = await _ApiClient.GetAsync($"{_BasePath}/random");
+
+            //// Assert
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            Assert.IsFalse(responseContent.Contains("\"statusCode\":"));
+            Assert.IsTrue(responseContent.Contains("\"StatusCode\":"));
+        }
+    }
+}
